6613a2e4762a8018fd35d4b41fe181dd4de9a990
GitHubKing23 <152663285+GitHubKing23@users.noreply.github.com>
2025-06-06 15:30:44 -0400

    Fix test file to use CommonJS syntax

--- src/components/__tests__/navbar-test.js
+++ src/components/__tests__/navbar-test.js
@@ -1,5 +1,5 @@
-import fs from 'fs';
-import path from 'path';
+const fs = require('fs');
+const path = require('path');
 
 describe('Navbar component', () => {
   it('includes all expected navigation links', () => {
