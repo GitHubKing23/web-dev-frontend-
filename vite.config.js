51ecfbdf57f2984296827d321b8d08af74bdffc6
GitHubKing23 <152663285+GitHubKing23@users.noreply.github.com>
2025-06-06 11:21:22 -0400

    Add MDX plugin and dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import mdx from '@mdx-js/rollup';
+import remarkFrontmatter from 'remark-frontmatter';
+import remarkMdxFrontmatter from 'remark-mdx-frontmatter';
 
 export default defineConfig({
-  plugins: [react(), mdx()],
+  plugins: [
+    react(),
+    mdx({
+      remarkPlugins: [
+        remarkFrontmatter,
+        [remarkMdxFrontmatter, { name: 'metadata' }]
+      ]
+    })
+  ],
   server: {
     historyApiFallback: true
   }
