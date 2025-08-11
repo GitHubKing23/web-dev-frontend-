9d64324f892c6054457f76fa5bda3e6e9219893b
Jeffrey <wise11jeff@gmail.com>
2025-08-11 00:48:40 +0000

    fix: make AI embed configurable + use hf.space

--- src/pages/AIToolPage.jsx
+++ src/pages/AIToolPage.jsx
@@ -1,5 +1,11 @@
+import React from "react";
 import WmpAiEmbed from "../components/WmpAiEmbed.jsx";
 
 export default function AIToolPage() {
-  return <WmpAiEmbed height={880} />;
+  return (
+    <WmpAiEmbed
+      height={880}
+      src="https://hf.space/embed/Jeff876/wmp-aii/+"
+    />
+  );
 }
